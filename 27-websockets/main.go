2cbee6bf8bc754c130a266896e5d663957aa7464
Lucas <lucas.silva@agencian1.com.br>
2024-08-20 23:09:55 -0300

    feat: fim do websocket

--- 27-websockets/main.go
+++ 27-websockets/main.go
@@ -1,64 +1,103 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
+	"time"
 
 	"github.com/coder/websocket"
 )
 
+type Client struct {
+	Nickname string
+	conn     *websocket.Conn
+	ctx      context.Context
+}
+
+type Message struct {
+	From    string
+	Content string
+	SentAt  string
+}
+
 var (
-	clients map[*websocket.Conn]bool = make(map[*websocket.Conn]bool)
+	clients     map[*Client]bool = make(map[*Client]bool)
+	joinCh      chan *Client     = make(chan *Client)
+	broadcastCh chan Message     = make(chan Message)
 )
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
+	nickname := r.URL.Query().Get("nickname")
+
+	conn, err := websocket.Accept(w, r, &websocket.AcceptOptions{
 		InsecureSkipVerify: true,
 	})
-
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 
-	// Cliente conectou com o websocket
+	go broadcast()
+	go joiner() //criando uma nova goroutine
 
-	clients[c] = true
+	//um cliente conectou com o websocket
+	newClient := Client{Nickname: nickname, conn: conn, ctx: r.Context()}
+	joinCh <- &newClient
 
-	for client := range clients {
-		client.Write(r.Context(), websocket.MessageText, []byte("Um novo cliente está conectado!"))
-	}
+	reader(&newClient)
+}
 
-	defer func() {
-		err := c.Close(websocket.StatusNormalClosure, "Conexão encerrada normalmente")
-		if err != nil {
-			log.Println("Erro ao fechar a conexão:", err)
-		}
-	}()
-
+func reader(newClient *Client) {
 	for {
-		_, data, err := c.Read(r.Context())
-
+		_, data, err := newClient.conn.Read(newClient.ctx)
 		if err != nil {
-			log.Println("Erro ao ler dados:", err)
-			delete(clients, c)
+			log.Println("Encerrando conexão do cliente")
+			delete(clients, newClient)
+			broadcastCh <- Message{From: newClient.Nickname, Content: newClient.Nickname + " saiu do chat", SentAt: time.Now().Format("02-01-2006 15:04:05")}
 			break
 		}
 
-		log.Print("Recebido: ", string(data))
+		var msgRec Message
+		json.Unmarshal(data, &msgRec)
+
+		broadcastCh <- Message{From: msgRec.From, Content: msgRec.Content, SentAt: time.Now().Format("02-01-2006 15:04:05")}
+	}
+}
+
+func joiner() {
+	for newClient := range joinCh {
+		clients[newClient] = true
+
+		broadcastCh <- Message{From: newClient.Nickname, Content: "O usuário " + newClient.Nickname + " se conectou", SentAt: time.Now().Format("02-01-2006 15:04:05")}
+	}
+}
+
+func broadcast() {
+	for newMsg := range broadcastCh {
 		for client := range clients {
-			client.Write(r.Context(), websocket.MessageText, data)
+			msg, _ := json.Marshal(newMsg)
+			client.conn.Write(client.ctx, websocket.MessageText, msg)
 		}
 	}
 }
 
 func main() {
+
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+
 	http.HandleFunc("/ws", wsHandler)
 
 	http.HandleFunc("/clients", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(strconv.Itoa(len(clients))))
+		w.Header().Add("Content-Type", "application/json")
+		var res []*Client
+		for c := range clients {
+			res = append(res, c)
+		}
+		json.NewEncoder(w).Encode(res)
 	})
 
 	http.ListenAndServe(":8080", nil)
+	// err := http.ListenAndServeTLS(":8080", "server.crt", "server.key", nil)
+	// fmt.Println(err)
 }
